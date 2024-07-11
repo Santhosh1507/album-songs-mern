57a09b0c7c0103d6cf98fac8a50b81cf059a8432
Santhosh1507 <webdesign1507@gmail.com>
2024-07-11 14:28:02 +0530

    first commit

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ import SongRouter from './routes/songRoute.js';
 import connectDB from './config/db.js';
 import connectCloudinary from './config/cloudinary.js';
 import albumRouter from './routes/albumRoute.js';
-import path from 'path';
 
 const app = express();
 const port = process.env.PORT;
@@ -15,17 +14,13 @@ app.use(express.json());
 connectDB();
 connectCloudinary();
 
-const __dirname = path.resolve();
-
 app.use('/api/song',SongRouter)
 app.use('/api/album',albumRouter)
 
 
-app.use(express.static(path.join(__dirname, '/admin/dist')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'admin', 'dist', 'index.html'));
-});
+app.get('/',(req,res)=>{
+    res.send("Api working")
+})
 
 app.listen(port,()=>{
     console.log(`Server is running on http://localhost:${port}`)
